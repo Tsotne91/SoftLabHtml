f80592729b18bf3e47ca12bc3818cb87001011d2
Tsotne91 <tsotne91@gmail.com>
2021-12-20 23:56:41 +0400

    Exercise 7 w/methods

--- js/7.js
+++ js/7.js
@@ -1,8 +1,6 @@
    const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
-   const arr = text.split(" ");
-    let result="";
-    for (let x of arr){
-        result=result+(x[0].toUpperCase());
-    }
-    console.log(result);
+   const result = text.split(" ")
+                       .map(word => word[0].toUpperCase())
+                       .reduce((acc, char) => acc+char);
+   console.log(result);
 
