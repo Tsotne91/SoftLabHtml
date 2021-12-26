fd8ee43c1b9e824c88453f9ca7328359c8212496
Tsotne91 <tsotne91@gmail.com>
2021-12-26 23:09:34 +0400

    Finished Exercise 13 and tests;

--- js/src/13.js
+++ js/src/13.js
@@ -1,19 +1,24 @@
-function isPrime (number){
-    if (number <=1 ) console.log('no prime numbers');
-    if (number===2) console.log("2");
-    else if (number===3){
-        console.log("2");
-        console.log("3");
+function isPrime (number) {
+    if (number == null) return ('enter a valid number');
+    else if (!(Number.isInteger(number))) return ('invalid input type');
+    else if (number <= 1) return ('no prime numbers');
+    else if (number === 2) return ("2");
+    else if (number === 3) {
+        return ("2 3");
     } else {
-        console.log("2");
-        console.log("3");
-        let count=0;
-            for (let i=4; i<=number; ++i, count=0){
-                for (let j=2; j<i; ++j) {
+        const higherNum = () => {
+            let result ='';
+            let count = 0;
+            for (let i = 4; i <= number; ++i, count = 0) {
+                for (let j = 2; j < i; ++j) {
                     if (i % j === 0) ++count;
                 }
-                if (count===0) console.log(i);
+                if (count === 0) result += i + '\n';
+
+                }
+            return ("2\n3\n" + result);
             }
-    }
+            return higherNum();
+        }
 }
 module.exports = isPrime;
