b71e90619f41f10c9fdb25d81f5e7014030b8e71
Tsotne91 <tsotne91@gmail.com>
2021-12-26 21:18:13 +0400

    Finished Exercise 13. Starting tests for it.

--- js/src/13.js
+++ js/src/13.js
@@ -7,17 +7,13 @@ function isPrime (number){
     } else {
         console.log("2");
         console.log("3");
-
-            for (let i=4; i<number; ++i){
+        let count=0;
+            for (let i=4; i<=number; ++i, count=0){
                 for (let j=2; j<i; ++j) {
-                    if (i % j === 0) {
-                        ++i;
-                    }
-                    console.log(i);
+                    if (i % j === 0) ++count;
                 }
+                if (count===0) console.log(i);
             }
     }
 }
-
-
-isPrime(21);
+module.exports = isPrime;
