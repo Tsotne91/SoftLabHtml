7817e5e8a58a89e8732eb22132088c50c24d7c12
Tsotne91 <tsotne91@gmail.com>
2021-12-30 01:19:40 +0400

    working on BootStrap project 29.12

--- js/src/index.js
+++ js/src/index.js
@@ -1,12 +1,1 @@
-function reverseNumber (N){
-    let C, D=1, P=0;
-    while(N) {
-        debugger
-        C = N%10;
-        N=Math.floor(N/10);
-        P+=C*D;
-        D*=10;
-    }
-    return P;
-}
-console.log(reverseNumber(123));
+console.log("this is index.js")
