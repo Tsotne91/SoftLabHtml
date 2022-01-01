6476991ec382f132af23a85dd491c30f889882d2
Tsotne91 <tsotne91@gmail.com>
2022-01-01 21:48:55 +0400

    Exer 17

---
+++ js/src/17.js
@@ -0,0 +1,8 @@
+function palindrome (word){
+    const end = word.substr(word.length/2 + word.length%2);
+    const begin = word.substr(0, word.length/2);
+    for (let i = 0; i < begin.length; i++) {
+        if (begin[i] !== end[end.length - 1 - i]) return false;
+    }
+    return true;
+}
