5c9ec435faac0e03d0b9cdda1cacd9ea1dfd2952
Tsotne91 <tsotne91@gmail.com>
2022-01-01 16:47:47 +0400

    Exer 16 incomplete.

--- js/src/index.js
+++ js/src/index.js
@@ -1,10 +1,1 @@
-function pal(x) {
-    let first_half = x.substr(0, x.length/2);
-    let second_half = x.substr(x.length/2 + x.length%2);
-    for (let i = 0; i < first_half.length; i++) {
-        if (first_half[i] !== second_half[second_half.length - 1 - i]) return false;
-    }
-    return true;
-}
-
-console.log(pal("abcacba"));
+console.log("hello world");
