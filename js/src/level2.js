cd78ff66357a312b168859475fa31b091a6d8c6a
Tsotne91 <tsotne91@gmail.com>
2022-05-30 22:52:36 +0400

    Module exports not working

--- js/src/level2.js
+++ js/src/level2.js
@@ -1,4 +1,4 @@
-import Point from '/home/tsotne/WebstormProjects/SoftLabHtml/js/jsClasses/level1'
+import Point from '/home/tsotne/WebstormProjects/SoftLabHtml/js/src/level1'
 
 class Line extends Point{
 
