6d69e7b9ff57ac71861691fde9626889feb7e151
Tsotne91 <tsotne91@gmail.com>
2022-06-13 00:38:49 +0400

    First Draft

--- Workshop2022June/src/PhotoModal.js
+++ Workshop2022June/src/PhotoModal.js
@@ -10,7 +10,7 @@ export default function PhotoModal(props){
             aria-labelledby="contained-modal-title-vcenter"
             centered
         >
-            <img src={props.photoUrl} alt="albumPhoto" className="w-50"/>
+            <img src={props.photoUrl} alt="albumPhoto" className="w-75 p-2 align-self-center"/>
             <Modal.Footer>
                 <Button onClick={props.onHide}>Close</Button>
             </Modal.Footer>
